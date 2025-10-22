02d960d1a9c1a0efb66c06e7db3654bfe0525cde
ZoidsKenji <chunkinmak123@gmail.com>
2025-10-22 19:30:19 +0100

    week 3

--- Week3.cs
+++ Week3.cs
@@ -4,23 +4,48 @@ using System.Globalization;
 
 public class Week3
 {
-    public List<float> dice = new List<float>();
+    public List<int> dice = new List<int>();
     public float count = 0;
+
+    public bool compareList(List<int> list1, List<int> list2)
+    {
+        if (list1.Count != list2.Count)
+        {
+            return false;
+        }
+        else
+        {
+            list1.Sort();
+            list2.Sort();
+            if (list1 != list2)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
     public void run()
     {
 
-        for (int i = 11111; i < 666666; i++)
+        for (int i = 11111; i <= 66666; i++)
         {
-            if (!i.ToString().Contains("0"))
+            if (!(i.ToString().Contains("0") || i.ToString().Contains("7") || i.ToString().Contains("8") || i.ToString().Contains("9")))
             {
                 count += 1;
+
+                if (i.ToString().Contains("2") && i.ToString().Contains("5"))
+                {
+                    dice.Add(i);
+                }
             }
-            if (i % 25 == 0 && !i.ToString().Contains("0"))
-            {
-                dice.Add(i);
-            }
+
         }
 
         Console.WriteLine(dice.Count / count);
+
+
     }
 }
